ae0607c2a55afc8546df5c6135bd13b4e256e09d
Ranim <i502848@fhict.nl>
2022-12-16 01:55:58 +0100

    d

--- sw.js
+++ sw.js
@@ -1,18 +1,14 @@
 self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("statics").then(cache => {
-            return cache.addAll(["./", "./src/style.css", "./image/logo192.png"]);
-        })
-    );
+    console.log("install!");
 });
 
- self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(response => {
-           return response || fetch(e.request);
-       })
-     );
-});
+//  self.addEventListener("fetch", e => {
+//     e.respondWith(
+//         caches.match(e.request).then(response => {
+//            return response || fetch(e.request);
+//        })
+//      );
+// });
 //  self.addEventListener("fetch", e => {
 //     console.log(`Intercepting fetch request for: ${e.request.url}`);
 //  })
