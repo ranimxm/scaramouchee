3195cb5d0a3c5d23b15746646b20f502c6c5f71d
Ranim <i502848@fhict.nl>
2022-12-18 13:42:20 +0100

    g

--- sw.js
+++ sw.js
@@ -1,16 +1,23 @@
 self.addEventListener("install", e => {
-    console.log("install dcode");
-    
+    e.waitUntil(
+        caches.open("statics").then(cache => {
+           return cache.addAll([
+                "./", 
+               "./src/style.css", 
+               "./image/sa.png",
+           ]);
+       })
+    );    
 
 
 });
- self.addEventListener("fetch", e => {
-         console.log(`Intercepting fetch request for: ${e.request.url}`);
-  });
-    // self.addEventListener("fetch", e => {
-    //   e.respondWith(
-    //        caches.match(e.request).then(response => {
-    //          return response || fetch(e.request);
-    //       })
-    //     );
-    //   });
+//  self.addEventListener("fetch", e => {
+//          console.log(`Intercepting fetch request for: ${e.request.url}`);
+//   });
+     self.addEventListener("fetch", e => {
+       e.respondWith(
+           caches.match(e.request).then(response => {
+              return response || fetch(e.request);
+           })
+         );
+       });
