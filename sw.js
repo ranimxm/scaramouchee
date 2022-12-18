59444ff704a3bcae21ca3c90cdf86d69102196e8
Ranim <i502848@fhict.nl>
2022-12-18 13:38:00 +0100

    d

--- sw.js
+++ sw.js
@@ -13,13 +13,13 @@ self.addEventListener("install", e => {
 
 
 });
-// self.addEventListener("fetch", e => {
-//     console.log(`Intercepting fetch request for: ${e.request.url}`);
-//  });
-    self.addEventListener("fetch", e => {
-      e.respondWith(
-           caches.match(e.request).then(response => {
-             return response || fetch(e.request);
-          })
-        );
-      });
+ self.addEventListener("fetch", e => {
+         console.log(`Intercepting fetch request for: ${e.request.url}`);
+  });
+    // self.addEventListener("fetch", e => {
+    //   e.respondWith(
+    //        caches.match(e.request).then(response => {
+    //          return response || fetch(e.request);
+    //       })
+    //     );
+    //   });
