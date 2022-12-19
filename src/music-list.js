7cbf3ef1e54791039003457b99dca30eab8fd9e0
Ranim <i502848@fhict.nl>
2022-12-20 00:49:21 +0100

    f

--- src/music-list.js
+++ src/music-list.js
@@ -9,8 +9,8 @@ let allMusic = [
   {
     name: "Scaramouche Boss Theme Phase 2",
     artist: "Genshin Impact",
-    img: "music-1",
-    src: "music-1"
+    img: "music-2",
+    src: "music-2"
   }
   
 ];
